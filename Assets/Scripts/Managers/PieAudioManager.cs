a1c2bce7f97e82a600095fb39eeccdfbcc614c60
Pollux-inge <152087053+Pollux-inge@users.noreply.github.com>
2023-12-02 18:59:23 +0100

    WallJump Sound Test

--- Assets/Scripts/Managers/PieAudioManager.cs
+++ Assets/Scripts/Managers/PieAudioManager.cs
@@ -7,10 +7,12 @@ public class PieAudioManager : MonoBehaviour
     private FMOD.Studio.EventInstance jumpinstance;
     private FMOD.Studio.EventInstance hurtinstance;
     private FMOD.Studio.EventInstance stepinstance;
+    private FMOD.Studio.EventInstance walljumpinstance;
 
     public string JumpEvent;
     public string HurtEvent;
     public string StepEvent;
+    public string WallJumpEvent;
 
     public PlayerCharacterController characterController;
     public PlayerAbilities playerAbilities;
@@ -22,6 +24,8 @@ public class PieAudioManager : MonoBehaviour
     public float counter = 0f;
     public float speed ;
 
+    public bool canwallsound = true;
+
     
 
     void Start()
@@ -29,6 +33,7 @@ public class PieAudioManager : MonoBehaviour
         jumpinstance = FMODUnity.RuntimeManager.CreateInstance(JumpEvent);
         hurtinstance = FMODUnity.RuntimeManager.CreateInstance(HurtEvent);
         stepinstance = FMODUnity.RuntimeManager.CreateInstance(StepEvent);
+        walljumpinstance = FMODUnity.RuntimeManager.CreateInstance(StepEvent);
         
     }
 
@@ -83,7 +88,26 @@ public class PieAudioManager : MonoBehaviour
 
         }
 
-        
+
+        if (playerAbilities.ability == AbilityEnum.WallJump)
+        {
+
+          if (characterController.CurrentCollisions.WallRight ||
+              characterController.CurrentCollisions.WallLeft )
+          {
+            
+              walljumpinstance.start();
+              
+            
+          }
+
+          if (characterController.CurrentCollisions.WallRight == false &&
+              characterController.CurrentCollisions.WallLeft == false )
+          {
+              walljumpinstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+             
+          }
+        }
 
        
     }
