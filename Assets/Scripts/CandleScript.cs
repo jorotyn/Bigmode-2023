cb303415e67066d4b74fd3a557843da224aa273c
calvin <calvin@calvins-MacBook-Pro-2.local>
2023-12-14 17:29:14 -0500

    basic candle attack anim

--- Assets/Scripts/CandleScript.cs
+++ Assets/Scripts/CandleScript.cs
@@ -1,18 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class CandleScript : MonoBehaviour
 {
     private PlayerCharacterController _characterController;
+    private Animator _animator;
     private Vector3 _velocity = Vector3.zero;
     private float _velocity_x_smoothing;
+
     [SerializeField] private float MoveSpeed = 6f;
+    [SerializeField] private GameObject ProjectilePrefab;
+    [SerializeField] private float ProjectileVelocity = 1f;
 
     private void Awake()
     {
         _characterController = GetComponent<PlayerCharacterController>();
+        _animator = GetComponent<Animator>();
+        StartCoroutine(WindUp());
     }
 
-    // Update is called once per frame
+    IEnumerator WindUp()
+    {
+        yield return new WaitForSeconds(3);
+        _animator.SetBool("WindingUp", true);
+    }
+
     void Update()
     {
         HandleMovement((Vector2)_velocity);
@@ -27,4 +39,33 @@ public class CandleScript : MonoBehaviour
         _characterController.Move(_velocity * Time.deltaTime);
     }
 
+    public void Fire()
+    {
+        DoCircleAttack();
+    }
+
+    private void DoCircleAttack()
+    {
+        const int radius = 2;
+
+        var numberOfProjectiles = 8;
+
+        var startPoint = (Vector2)transform.position;
+        float angleStep = 360f / numberOfProjectiles;
+        float angle = 0f;
+
+        for (int i = 0; i <= numberOfProjectiles - 1; i++)
+        {
+            var projectileDirXPosition = startPoint.x + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+            var projectileDirYPosition = startPoint.y + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+
+            var projectileVector = new Vector2(projectileDirXPosition, projectileDirYPosition);
+            var projectileMoveDirection = (projectileVector - startPoint).normalized * ProjectileVelocity;
+
+            var tmpObj = Instantiate(ProjectilePrefab, startPoint, Quaternion.identity);
+            tmpObj.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileMoveDirection.x, projectileMoveDirection.y);
+
+            angle += angleStep;
+        }
+    }
 }
